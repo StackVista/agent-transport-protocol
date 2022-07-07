86353f8c12b1865abb34c442be0f712befd6bae5
Juliano Krieger <julianosk@gmail.com>
2022-07-07 15:39:29 +0200

    Expose CheckPayload properties

--- pkg/model/check_payload.go
+++ pkg/model/check_payload.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type CheckPayload struct {
-	messages  []MessageBody
-	endpoint  string
-	timestamp time.Time
+	Messages  []MessageBody
+	Endpoint  string
+	Timestamp time.Time
 }
